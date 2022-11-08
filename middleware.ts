ffc9eef32608c69cbb2f8e94705f52540806f03f
David Cramer <dcramer@gmail.com>
2022-11-07 22:28:18 -0800

    Maybe this helps

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
 export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: [
+    "/",
+    "/:username*",
+    "/explore",
+    "/bookmarks",
+    "/notifications",
+    "/messages",
+  ],
+};
